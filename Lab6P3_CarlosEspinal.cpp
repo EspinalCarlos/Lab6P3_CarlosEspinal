9b2763cbfa4e4f297f9fee41985f00ccce4988b8
Carlos Espinal <cespinalb@gmail.com>
2023-08-25 13:03:59 -0600

    Primer commit

---
+++ Lab6P3_CarlosEspinal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void menu() {
+    cout << "---- MENU PRINCIPAL ---- \n" << "1. Agregar Competidores \n" << "2. Ver Competidores \n" << "3. Simular competencias \n"<< "4. Salir \n";
+}
+
+void menucomp() {
+    cout << "";
+}
+
+int main(){
+    
+}
